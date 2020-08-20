d09216a3dd9d2910f6fb0c36df7731245acce938
mliskPSU <57715100+mliskPSU@users.noreply.github.com>
2020-08-20 11:25:46 -0400

    updating radar processing script

--- scripts/radar/scrapPrepRadar.R
+++ scripts/radar/scrapPrepRadar.R
@@ -27,7 +27,13 @@ if(dir.exists(downDir)==F){dir.create(downDir, recursive=T)}
 if(dir.exists(clipDir)==F){dir.create(clipDir, recursive=T)}
 
 #Viewport bounds plus a little extra
-cropEx <- extent(c(-85.0, -70.0, 33.0, 46.5))
+cropEx <- extent(c(-85.0, -70.0, 33.0, 46.5))  ##actual coordinates
+#tempRast <- raster("/Users/mdl5548/Documents/GitHub/marisa-map-backup/scripts/radar/downloads/n0q_202008171730.tif")
+#northRow <- rowFromY(tempRast, 46.5)
+#southRow <- rowFromY(tempRast, 33.0)
+#eastRow <- colFromX(tempRast, -70.0)
+#westRow <- colFromX(tempRast, -85.0)
+#cropEx <- extent(c(8201, 11201, 701, 3401))  ##for png, if needed
 
 ##site indentity
 baseURL <- "http://mesonet.agron.iastate.edu"
@@ -41,6 +47,8 @@ getNodes <- html_nodes(webpage, 'a')
 getText <- html_text(getNodes)
 
 GTLinks <- rev(grep("GeoTiff", getText))  ##original order is oldest first, newest last
+#GTLinks <- rev(grep("n0q_", getText))
+
 subLinks <- GTLinks[1:(length(GTLinks)-2)]
 layerNames <- c("minus0", "minus5", "minus10", "minus15", "minus20", "minus25", "minus30", "minus35", "minus40", "minus45", "minus50")
 
@@ -61,14 +69,21 @@ if(cores>1){
   pbmapply(function(lnk, ln){getDatLink <- paste0(baseURL, html_attr(getNodes[lnk], "href"))
                               linkID <- sapply(strsplit(getDatLink, "="), "[[", 2)
                               downloadName <- paste0(downDir, "dlRad_", ln, ".zip")
+                              #linkID <- sapply(strsplit(getDatLink, "n0q_|.png"), "[[", 2)
+                              #downloadName <- paste0(downDir, "dlRad_", ln, ".png")
                               download.file(getDatLink, downloadName)
                               unzip(downloadName, exdir=downDir)
                               findUnzip <- list.files(downDir, linkID, full.names=T)
+                              #findUnzip <- list.files(downDir, ln, full.names=T)
                               rast <- raster(findUnzip)
                               names(rast) <- ln
                               crRast <- crop(rast, cropEx)
                               writeRaster(crRast, paste0(clipDir, "cropped_", ln, ".tif"), overwrite=T)}, lnk=subLinks, ln=layerNames)
 }  
+
+listTif <- list.files(downDir, ".tif", full.names=T)
+sapply(listTif, function(fn){if(file.exists(fn)){file.remove(fn)}})
+
 
 
 
