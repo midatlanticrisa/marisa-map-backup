dd2748abc49775457a947f465baebba579fac6ea
mliskPSU <57715100+mliskPSU@users.noreply.github.com>
2020-09-15 11:15:29 -0400

    added print in order to find script error 2

--- scripts/warnings_watches_advisories/NWS_AlertParseFunc.R
+++ scripts/warnings_watches_advisories/NWS_AlertParseFunc.R
@@ -37,19 +37,21 @@ library(stringr)
 # Function extracting weather data from an XML file.
 parse_xml = function(ID){
   #################
-  #ID <- 
+  #ID <- "OHC057"
   #################
   print(ID)
   url = paste("https://alerts.weather.gov/cap/wwaatmget.php?x=", ID, "&y=1", sep="")
 
   # Turn XML data into a list.
   xml_data <- xmlToList(rawToChar(GET(url)$content))
-
+  print("collected data")
   name <- xml_data$title
   entry <- xml_data$entry$title
   link <- xml_data$entry$id
+  print("parsed data")
 
   if(entry == "There are no active watches, warnings or advisories"){
+    print("no warnings")
     cols = "#00000000" # 100% transparent black
 
     time <- xml_data$updated
@@ -61,14 +63,15 @@ parse_xml = function(ID){
     OBS<- paste("<strong><a href='", link, "' target='_blank'>", name, "</a></strong><br/><br/><strong>", entry, "</strong><br/><br/>Last Updated on ", format_time, sep="")
 
   } else {
+    print("active warning")
     xml_active <- xmlToList(rawToChar(GET(link)$content))
-
+    print("parse content")
     time <- xml_data$updated
-
+    
     # Reformat time to match the rest of the Marisa data
     format_time <- as.POSIXct(str_replace_all(time, "T", " "), format = "%Y-%m-%d %H:%M", tz = "EST8EDT")
     format_time <- format(format_time, format = "%b %d, %Y %I:%M %p %Z", tz = "EST8EDT")
-
+    print("updated time")
     # headline <- xml_active$info$headline
     event <- xml_active$info$event
     issued <- xml_active$sent
@@ -78,24 +81,25 @@ parse_xml = function(ID){
     description <- xml_active$info$description
     instructions <- xml_active$info$instruction
     areas_affected <- xml_active$info$area$areaDesc
-
+    print("parse data some more")
     cols <- as.character(NWS_cols[match(event, NWS_cols[ ,1]), 2])
-
+    print("formatted output")
     # Reformat time to match the rest of the Marisa data
     format_issued <- as.POSIXct(str_replace_all(issued, "T", " "), format = "%Y-%m-%d %H:%M", tz = "America/New_York")
     format_issued <- format(format_issued, format = "%b %d, %Y %I:%M %p %Z")
     format_expiring <- as.POSIXct(str_replace_all(expiring, "T", " "), format = "%Y-%m-%d %H:%M", tz = "America/New_York")
     format_expiring <- format(format_expiring, format = "%b %d, %Y %I:%M %p %Z")
-
+    print("formatted time")
     OBS<- paste("<strong><a href='", link, "' target='_blank'>", name, "</a></strong><br/><br/><strong>", event,
                 "</strong><br/><strong>Issued: </strong>", format_issued, "<br/><strong>Expires: </strong>", format_expiring,
                 "<br/><strong>Severity: </strong><br/><br/><strong>Alert: </strong>", description,
                 "<br/><br/><strong>Instructions: </strong>", instructions, "<br/><br/><strong>Areas affected: </strong>", areas_affected,
                 "<br/><br/>Last Updated on ", format_time, sep="")
-
+    print("create observation")
     OBS<- str_replace_all(OBS, "([\n])([*])", "<br/>*")
     OBS<- str_replace_all(OBS, "([\n])", " ")
     OBS<- str_replace_all(OBS, "([\"])", " inches")
+    print("observation set")
   }
   return(c(ID, OBS, cols))
 }
